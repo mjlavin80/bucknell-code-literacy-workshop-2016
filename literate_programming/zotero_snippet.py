335918e1d0f3deb6053431d76e4ffac20efd07a4
mjlavin80 <lavin@pitt.edu>
2016-10-27 16:05:25 -0400

    snippet

---
+++ literate_programming/zotero_snippet.py
@@ -0,0 +1,21 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+from pyzotero import zotero
+from collections import Counter
+
+library_id = "655084"
+library_type = "group"
+api_key= "ajdUMPwUpwULGD2cwdXaOASG"
+
+zot = zotero.Zotero(library_id, library_type, api_key)
+hybrid_blended = zot.everything(zot.collection_items("95HW46ZG"))
+dates = [int(i["data"]["date"]) for i in hybrid_blended]
+
+years = [d for d in set(dates)]
+years.sort()
+counts = Counter([u for u in dates])
+articles = [counts[y] for y in years]
+
+plt.figure(figsize=(20,10))
+plt.plot(years[4:], articles[4:])
+plt.show()
